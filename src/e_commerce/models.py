060b7c7c575a73e92e82ff1b6b3519c30a86df39
mehmetbarispolat <mehmetbarispolat@gmail.com>
2024-02-18 00:39:12 +0000

    [COMM-9] Adjustment of relationships in Product, ProductStock and SalesChannel models.

--- src/e_commerce/models.py
+++ src/e_commerce/models.py
@@ -1,28 +1,42 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class ProductType(models.TextChoices):
+    SINGLE = "single", _("Single")
+    BUNDLE = "bundle", _("Bundle")
+
+
+class SalesChannel(models.Model):
+    TRENDYOL = "trendyol"
+    HEPSIBURADA = "hepsiburada"
+    SALES_CHANNEL_NAMES = [
+        (TRENDYOL, "trendyol"),
+        (HEPSIBURADA, "hepsiburada"),
+    ]
+    name = models.CharField(max_length=100, choices=SALES_CHANNEL_NAMES, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductStock(models.Model):
+    quantity = models.IntegerField(default=0)
 
 
 class Product(models.Model):
-    SINGLE = "single"
-    BUNDLE = "bundle"
-    PRODUCT_TYPE_CHOICES = [
-        (SINGLE, "Single"),
-        (BUNDLE, "Bundle"),
-    ]
-
     name = models.CharField(max_length=255)
     description = models.TextField()
-    sales_channel = models.CharField(max_length=255)
+    sales_channel = models.ForeignKey(
+        SalesChannel, on_delete=models.CASCADE, related_name="products"
+    )
     price = models.DecimalField(max_digits=10, decimal_places=2)
     product_type = models.CharField(
-        max_length=10, choices=PRODUCT_TYPE_CHOICES, default=SINGLE
+        max_length=20, choices=ProductType.choices, default=ProductType.SINGLE
     )
     bundled_products = models.ManyToManyField(
         "self", symmetrical=False, blank=True, related_name="bundles"
     )
-
-
-class ProductStock(models.Model):
-    product = models.OneToOneField(
-        Product, on_delete=models.CASCADE, related_name="stock"
+    stock = models.ForeignKey(
+        ProductStock, on_delete=models.CASCADE, related_name="stock"
     )
-    quantity = models.IntegerField(default=0)
--- src/e_commerce/serializers.py
+++ src/e_commerce/serializers.py
@@ -1,16 +1,28 @@
+from django.http import Http404
 from rest_framework import serializers
-from .models import Product, ProductStock
+
+from .models import Product, ProductStock, SalesChannel, ProductType
+
+
+class SalesChannelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SalesChannel
+        fields = ["id", "name"]
 
 
 class ProductStockSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductStock
-        fields = ["quantity"]
+        fields = ["id", "quantity"]
 
 
 class ProductSerializer(serializers.ModelSerializer):
     stock_quantity = serializers.IntegerField(write_only=True, required=False)
+    sales_channel_name = serializers.ChoiceField(
+        choices=SalesChannel.SALES_CHANNEL_NAMES, write_only=True, required=True
+    )
     stock = ProductStockSerializer(read_only=True)
+    sales_channel = SalesChannelSerializer(read_only=True)
 
     class Meta:
         model = Product
@@ -24,18 +36,40 @@ class ProductSerializer(serializers.ModelSerializer):
             "bundled_products",
             "stock_quantity",
             "stock",
+            "sales_channel_name",
         ]
 
-    def create(self, validated_data):
-        stock_quantity = validated_data.pop("stock_quantity", 0)
-        product = Product.objects.create(**validated_data)
+    def create(self, validated_data: dict):
+        stock_quantity: int = validated_data.pop("stock_quantity", 0)
+        sales_channel_name: str = validated_data.pop("sales_channel_name", "")
+        sales_channel, _ = SalesChannel.objects.get_or_create(name=sales_channel_name)
 
-        if product.product_type == Product.SINGLE:
-            ProductStock.objects.create(product=product, quantity=stock_quantity)
-        elif product.product_type == Product.BUNDLE:
-            min_stock = min(
-                [p.stock.quantity for p in product.bundled_products.all()], default=0
+        # TODO: Write as declarative
+        product = Product(**{**validated_data, "sales_channel": sales_channel})
+
+        # TODO: Add same product_id for trendyol, hepsiburada into stock
+        try:
+            # Trendyol 3 geldi hepsiburada 3
+            # TODO: Add SKU into product because name is not unique
+            product_from_different_channel = Product.objects.get(
+                name=validated_data.get("name")
             )
-            ProductStock.objects.create(product=product, quantity=min_stock)
+            if product_from_different_channel.stock.quantity != stock_quantity:
+                # TODO: Add specific exception handler
+                raise Http404
+            product.stock = ProductStock.objects.get(
+                id=product_from_different_channel.stock.id
+            )
+        except Product.DoesNotExist:
+            if product.product_type == ProductType.SINGLE:
+                product.stock = ProductStock.objects.create(quantity=stock_quantity)
+            elif product.product_type == ProductType.BUNDLE:
+                min_stock = min(
+                    [p.stock.quantity for p in product.bundled_products.all()],
+                    default=0,
+                )
+                product.stock = ProductStock.objects.create(quantity=min_stock)
+
+        product.save()
 
         return product
